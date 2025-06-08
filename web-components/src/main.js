febe20e869b022aa58a7bfd45cb302dbb8eccfda
jojopalma <jojodevelop@proton.me>
2025-06-08 13:43:45 -0400

    eliminate counter

--- web-components/src/main.js
+++ web-components/src/main.js
@@ -5,14 +5,19 @@ import webComponentsLogo from './webcomponents.svg'
 document.querySelector('#app').innerHTML = `
   <div>
     <a href="/native-components" target="_blank">
-    <h1>Demo Web Components</h1>
-    <img src="${webComponentsLogo}" class="logo" alt="Web Components logo" />
+      <img src="${webComponentsLogo}" class="logo" alt="Web Components logo" />
     </a>
     <a href="/lit-components" target="_blank">
-    <img src="${litLogo}" class="logo lit" alt="Lit logo" />
+      <img src="${litLogo}" class="logo lit" alt="Lit logo" />
     </a>
+    <h1>Web Components Demo</h1>
+    <div class="card">
+      <button id="counter" type="button"></button>
+    </div>
     <p class="read-the-docs">
-      Clickea una imagen para ver los componentes web nativos y desarrollados con Lit.
+      Click on the Web Components logo to see native components or Lit logo to see Lit components
     </p>
   </div>
 `
+
+setupCounter(document.querySelector('#counter'))
