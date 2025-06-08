3f625edc5b84623cb6facb00975881848c341c97
jojopalma <jojodevelop@proton.me>
2025-06-08 13:44:51 -0400

    Landing Page

--- web-components/src/main.js
+++ web-components/src/main.js
@@ -4,18 +4,15 @@ import webComponentsLogo from './webcomponents.svg'
 
 document.querySelector('#app').innerHTML = `
   <div>
+  <h1>Web Components Demo</h1>
     <a href="/native-components" target="_blank">
       <img src="${webComponentsLogo}" class="logo" alt="Web Components logo" />
     </a>
     <a href="/lit-components" target="_blank">
       <img src="${litLogo}" class="logo lit" alt="Lit logo" />
     </a>
-    <h1>Web Components Demo</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
     <p class="read-the-docs">
-      Click on the Web Components logo to see native components or Lit logo to see Lit components
+      Haz clic en las imágenes para ver los componentes web nativos y desarrollados con Lit.
     </p>
   </div>
 `
